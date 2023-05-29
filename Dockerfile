02fe00fb46177406bc7e728e5911e14d8aa68a62
Carmelo C <carmelo.califano@gmail.com>
2023-05-29 15:38:59 +0200

    Moved from dig+myip.opendns.com to curl+myipv4.p1.opendns.com

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.10
 ENV DESTDIR /usr/local/bin/
-RUN apk add --no-cache bind-tools openssh-client python3
+RUN apk add --no-cache curl openssh-client python3
 RUN mkdir /root/.ssh && chmod 700 /root/.ssh
 COPY main.py ${DESTDIR}
 WORKDIR ${DESTDIR}
--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-import subprocess, time
+import json, subprocess, time
 
 # Global settings
 thisHostname = subprocess.check_output(['hostname']).rstrip().decode('utf-8')
-get_name = 'myip.opendns.com'
-get_server = '@resolver1.opendns.com'
-remoteServer = '<user>@<remote_server>'      # replace with your own account
-waitTime = 3600                              # default interval, 3600s = 1h
-privateKey = '/root/.ssh/private.my'         # place your own private key inside the target file
-knownHosts = '/root/.ssh/known_hosts.my'     # dummy file, placeholder
+resolverURL = 'https://myipv4.p1.opendns.com/get_my_ip'
+remoteServer = '<user>@<remote_server>'                      # replace with your own account
+waitTime = 3600                                              # default interval, 3600s = 1h
+privateKey = '/root/.ssh/private.my'                         # place your own private key inside the target file
+knownHosts = '/root/.ssh/known_hosts.my'                     # dummy file, placeholder
 
 # Main function, infinite loop
 while True:
     # Fetch the current literal public IP, PHP: getenv("REMOTE_ADDR")
     # format: "x.y.w.z"
-    currIP = subprocess.check_output(['dig', '+short', get_server, get_name]).rstrip().decode('utf-8')
+#    currIP = subprocess.check_output(['dig', '+short', get_server, get_name]).rstrip().decode('utf-8')
+    currIP4 = subprocess.check_output(['curl', '-s', resolverURL])
+    currIP4_json = json.loads(currIP4)
+    currIP = currIP4_json['ip']
 
     # Let's build the string that we'll use to update the remote log
     outData = currIP + ';' + subprocess.check_output('date').rstrip().decode('utf-8') + ';' + thisHostname
-#    remote_ip_ts_hn = 'echo \"' + outData + '\" >> ~/WWW/files/raspi_publicIP'
     remote_ip_ts_hn = 'echo \"' + outData + '\" >> ~/SWWW/raspi_publicIP'
 
     # Append the latest info to the remote log, SSH will be 'password-less' if public key has been added to remoteServer's authorized_keys
